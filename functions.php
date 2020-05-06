c5b2386e8d3915bbca1ec780da9f97c6a07d6c5c
RaccoonFunny <62151481+RaccoonFunny@users.noreply.github.com>
2020-05-06 19:54:09 +0300

    Создал стартовые файлы

---
+++ functions.php
@@ -0,0 +1,12 @@
+function enqueue_styles() {
+	wp_enqueue_style( 'whitesquare-style', get_stylesheet_uri());
+	wp_register_style('font-style', 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap');
+	wp_enqueue_style( 'font-style');
+}
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+
+function enqueue_scripts () {
+	wp_register_script('html5-shim', 'http://html5shim.googlecode.com/svn/trunk/html5.js');
+	wp_enqueue_script('html5-shim');
+}
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
